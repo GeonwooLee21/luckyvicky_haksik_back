fc8bb184ee4d7fb78f6f94d358592c1cb8a3f52c
cjf1685 <cjf1685@gmail.com>
2025-11-17 09:41:54 +0900

    Init: spring project - luckyvicky
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/luckyvicky/hakshikmap/LuckyVickyHakshikMapApplication.java
@@ -0,0 +1,13 @@
+package com.luckyvicky.hakshikmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckyVickyHakshikMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckyVickyHakshikMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luckyvicky/hakshikmap/LuckyVickyHakshikMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckyvicky.hakshikmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyVickyHakshikMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
